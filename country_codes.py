4428f5329c6e1286bc971cfbfadfcfe3b58516ec
Ruslan Nazarov <hlfan2@yandex.ru>
2022-01-24 21:00:24 +0700

    PEP8 fix

--- country_codes.py
+++ country_codes.py
@@ -1,5 +1,6 @@
 from pygal.maps.world import COUNTRIES
 
+
 def get_country_code(country_name):
     """Возвращает для заданной страны ее код Pygal, состоящий из 2х букв."""
     for code, name in COUNTRIES.items():
