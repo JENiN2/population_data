4af8de6fc67cc78d10754cde64196ba9b062511e
Ruslan Nazarov <hlfan2@yandex.ru>
2022-01-24 21:10:22 +0700

    import get_country_code to world_population.py

--- world_population.py
+++ world_population.py
@@ -1,5 +1,7 @@
 import json
 
+from country_codes import get_country_code
+
 # Список заполняется данными.
 filename = "data/population_data.json"
 with open(filename) as f:
@@ -11,4 +13,8 @@ for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
-        print(country_name + ': ' + str(population))
+        code = get_country_code(country_name)
+        if code:
+            print(f'{code}: {str(population)}')
+        else:
+            print(f'ERROR - {str(population)}')
