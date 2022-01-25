17b97e184bd18f074fcac22c79530bd22dd82306
Ruslan Nazarov <hlfan2@yandex.ru>
2022-01-25 14:44:07 +0700

    interactive world_population

--- world_population.py
+++ world_population.py
@@ -1,4 +1,5 @@
 import json
+import pygal.maps.world
 
 from country_codes import get_country_code
 
@@ -7,14 +8,18 @@ filename = "data/population_data.json"
 with open(filename) as f:
     pop_data = json.load(f)
 
-print('World population in 2010:\n')
-# Вывод населения за 2010 год.
+# Построение словаря с данными численности населения за 2010 год.
+cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
         code = get_country_code(country_name)
         if code:
-            print(f'{code}: {str(population)}')
-        else:
-            print(f'ERROR - {str(population)}')
+            cc_populations[code] = population
+
+wm = pygal.maps.world.World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', cc_populations)
+
+wm.render_to_file('world_population.svg')
