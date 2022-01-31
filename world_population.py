b2f58aac1d47c9f51f91d21cf3b91de1a3e17032
Ruslan Nazarov <hlfan2@yandex.ru>
2022-01-31 12:21:01 +0700

    add choices to parser, del check year func

--- world_population.py
+++ world_population.py
@@ -11,18 +11,16 @@ from country_codes import get_country_code
 def get_year():
     parser = argparse.ArgumentParser(description='Creating of an interactive map, showing population by country.')
     parser.add_argument(
-        '--year', type=str, required=True, help='Enter year, from 1960 to 2010.'
+        '--year', type=int,
+        choices=[1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976,
+                 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993,
+                 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010],
+        required=True, help='Enter year, from 1960 to 2010.'
     )
     args = parser.parse_args()
     year = args.year
     print(f'You choose {year}')
     return year
-
-
-def check_year(year):
-    if int(year) > 2010 or int(year) < 1960:
-        print('Wrong year')
-        sys.exit()
 
 
 def get_population_data():
